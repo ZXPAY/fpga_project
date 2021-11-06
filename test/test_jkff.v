4d686d1c8435f4ed16787f50c7ff5772755cc340
Guillaume DannyDeng <zxpay73945788@gmail.com>
2021-11-06 20:16:48 +0800

    add the latex toolchain to project

--- test/test_jkff.v
+++ test/test_jkff.v
@@ -10,6 +10,8 @@ reg pr = 1;
 reg cl = 1;
 wire q, q_bar;
 
+reg [0:7] cnt = 0;
+
 initial begin
 	$dumpfile("test_jkff.vcd");
     $dumpvars;
@@ -19,10 +21,28 @@ jkff test_jkff(q, q_bar, j, k, clk, pr, cl);
 
 always #1 begin
 	clk = ~clk;
+
+	if(cnt == 5) begin
+		j <= 1'b1;
+		k <= 1'b0;
+	end
+	else if(cnt == 10) begin
+		j <= 1'b0;
+		k <= 1'b0;
+	end
+	else if(cnt == 20) begin
+		j <= 1'b0;
+		k <= 1'b1;
+	end
+	else if(cnt == 25) begin
+		cl <= 0;
+	end
+
+	cnt = cnt + 1;
 end
 
 
-initial #100 $finish;
+initial #30 $finish;
 endmodule
 
 
